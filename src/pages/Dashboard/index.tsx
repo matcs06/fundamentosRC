23d755b8fb4a409cc1d76c7a919d8ed155f952b4
Mateus Costa <matcs06@gmail.com>
2020-06-05 03:41:15 -0300

    React native fundamentals challenge

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -36,13 +36,17 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadProducts(): Promise<void> {
       // TODO
+      const response = await api.get('/products');
+
+      setProducts(response.data);
     }
 
     loadProducts();
-  }, []);
+  }, [products]);
 
   function handleAddToCart(item: Product): void {
     // TODO
+    addToCart(item);
   }
 
   return (
